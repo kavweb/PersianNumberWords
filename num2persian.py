11dc79f2d82f3bfef2a889816ecfd8f82a518751
kavweb <kavemahdiz@gmail.com>
2025-08-01 11:22:12 +0330

    put AND

--- num2persian.py
+++ num2persian.py
@@ -1,19 +1,26 @@
-data = {
-    "1":["یک","planB","صد"],
-    "2":["دو","بیست","دویست"],
-    "3":["سه","سی","سی صد"],
-    "4":["چهار","چهل","چهارصد"],
-    "5":["پنج","پنجاه","پانصد"],
-    "6":["شش","شصت","ششصد"]}
+planeA = {
+    "1":["صد","planB","یک"],
+    "2":["دویست","بیست","دو"],
+    "3":["سی صد","سی","سه"],
+    "4":["چهارصد","چهل","چهار"],
+    "5":["پانصد","پنجاه","پنج"],
+    "6":["ششصد","شصت","شش"]}
 planBData = ["ده","یازده","دوازده","سیزده","چهارده","پانزده","شانزده","هفده","هجده","نوزده"]
 module = ["هزار","میلیون","میلیارد"]
 finallText = ""
+usePB = False
 counter = 0
 userEntry = input("put you number to chenge it into the persian  ... ")
+def checkAnd(checkCounter):
+    if (checkCounter+1)%3!=0 or checkCounter==0:
+        return " و "
+    else:
+        return " "
 if userEntry:
     for item in userEntry:
-        if 0<=counter<=2 and len(userEntry) > 0:
-            tempNum = data[userEntry]
-            print(tempNum[counter])
-            counter+=1
-            
+        if 0<=counter<=2 and len(userEntry) > 0 and item != 0 :
+            tempNum = planeA[item][counter]
+            finallText = finallText + tempNum + checkAnd(counter)
+        counter+=1
+
+print(finallText)
