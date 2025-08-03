a47c9647885c3edcb56e71d8bb20e5929ce7c2d9
kavweb <kavemahdiz@gmail.com>
2025-08-03 16:06:55 +0330

    change the way and now we have array of Persian

--- num2persian.py
+++ num2persian.py
@@ -1,26 +1,46 @@
 planeA = {
-    "1":["صد","planB","یک"],
-    "2":["دویست","بیست","دو"],
-    "3":["سی صد","سی","سه"],
-    "4":["چهارصد","چهل","چهار"],
-    "5":["پانصد","پنجاه","پنج"],
-    "6":["ششصد","شصت","شش"]}
-planBData = ["ده","یازده","دوازده","سیزده","چهارده","پانزده","شانزده","هفده","هجده","نوزده"]
-module = ["هزار","میلیون","میلیارد"]
-finallText = ""
+    "1":["یک","planB","صد"],
+    "2":["دو","بیست","دویست"],
+    "3":["سه","سی","سی صد"],
+    "4":["چهار","چهل","چهارصد"],
+    "5":["پنج","پنجاه","پانصد"],
+    "6":["شش","شصت","ششصد"]}
+planB = ['ده', 'یازده', 'دوازده', 'سیزده', 'چهارده', 'پانزده', 'شانزده', 'هفده', 'هجده', 'نوزده']
+module = ["میلیارد","میلیون","هزار"]
+finallText = []
 usePB = False
+checkAnd = False
+
 counter = 0
 userEntry = input("put you number to chenge it into the persian  ... ")
-def checkAnd(checkCounter):
-    if (checkCounter+1)%3!=0 or checkCounter==0:
+def checkAndFunc(checkCounter):
+    print (checkAnd)
+    if checkCounter != 0 or checkAnd == False:
         return " و "
     else:
         return " "
+def addNumberToArray(number,counter):
+    changeNumberToP = planeA[number][counter]
+    finallText.append(changeNumberToP)
+    
 if userEntry:
-    for item in userEntry:
-        if 0<=counter<=2 and len(userEntry) > 0 and item != 0 :
-            tempNum = planeA[item][counter]
-            finallText = finallText + tempNum + checkAnd(counter)
-        counter+=1
-
+    userEntryLen = len(userEntry)
+    if userEntryLen%3==1:
+        tempNum = userEntry[0]
+        addNumberToArray(tempNum,counter=0)
+        userEntry = userEntry[1:]
+    elif userEntryLen%3==2:
+        tempNum = userEntry[0:2]
+        counter = 1
+        for num in tempNum:
+            addNumberToArray(num , counter)
+            counter -= 1
+        userEntry = userEntry[2:]
+    for i in range(int(len(userEntry)/3)):
+        tempNum = userEntry[0:3]
+        userEntry = userEntry [3:]
+        counter = 2
+        for itemA in tempNum:
+            addNumberToArray(itemA,counter)
+            counter-=1
 print(finallText)
